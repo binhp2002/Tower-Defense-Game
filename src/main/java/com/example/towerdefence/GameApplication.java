9cc0323624ff8faca0e365769548346e8bb2b4db
czming <chng.zheming@dhs.sg>
2021-10-01 19:07:42 -0400

    change screen size 320 x 240 -> 640 x 480

--- src/main/java/com/example/towerdefence/GameApplication.java
+++ src/main/java/com/example/towerdefence/GameApplication.java
@@ -12,7 +12,7 @@ public class GameApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(GameApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+        Scene scene = new Scene(fxmlLoader.load(), 640, 480);
         stage.setTitle("Hello World!");
         stage.setScene(scene);
         stage.show();
