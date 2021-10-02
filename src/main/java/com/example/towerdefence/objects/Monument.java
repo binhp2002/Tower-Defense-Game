490727ac557aaa342bffd8766abb651856d98bdc
czming <chng.zheming@dhs.sg>
2021-10-01 20:49:56 -0400

    add Monument class

---
+++ src/main/java/com/example/towerdefence/objects/Monument.java
@@ -0,0 +1,34 @@
+package com.example.towerdefence.objects;
+
+public class Monument {
+
+    private int health;
+
+    /**
+     * intialize Monument object with -1 health
+     */
+    public Monument() {
+        health = -1;
+    }
+
+    /**
+     * return health of monument
+     * @return monument's health
+     */
+    public int getHealth() {
+        return this.health;
+    }
+
+    /**
+     * sets health of monument (health must be non-negative)
+     * @param health new health of monument
+     * @return 0 if successfully changed health of monument, -1 otherwise
+     */
+    public int setHealth(int health) {
+        if (health == 0) {
+            return -1;
+        }
+        this.health = health;
+        return 0;
+    }
+}
