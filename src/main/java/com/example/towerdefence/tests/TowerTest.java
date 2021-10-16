98bcc762344cfc6cadf3e499cd0c3f2e5c81ecde
czming <chng.zheming@dhs.sg>
2021-10-16 16:39:47 -0400

    add javadocs for TowerTests

--- src/main/java/com/example/towerdefence/tests/TowerTest.java
+++ src/main/java/com/example/towerdefence/tests/TowerTest.java
@@ -17,58 +17,88 @@ public class TowerTest {
         machineTower = new MachineTower();
     }
 
+    /**
+     * checks that SniperTower shoot method returns a SniperProjectile object
+     */
     @Test
     public void testSniperTowerShoot() {
         //check that the correct projetile was fired
         assertTrue(sniperTower.shoot() instanceof SniperProjectile);
     }
 
+    /**
+     * checks that sniper tower get name method implemented correctly
+     */
     @Test
     public void testSniperGetName() {
         //check that SniperTower has correct name
         assertEquals( "Sniper Tower", sniperTower.getName());
     }
 
+    /**
+     * checks that sniper tower get basic cost runs successfully
+     */
     @Test
     public void testSniperGetBasicCost() {
         //ensure that no error when getting basic cost
         sniperTower.getBasicCost();
     }
 
+    /**
+     * checks that BasicTower shoots a NormalProjectile
+     */
     @Test
     public void testBasicTowerShoot() {
         assertTrue(basicTower.shoot() instanceof NormalProjectile);
     }
 
+    /**
+     * checks that BasicTower get name implemented correctly
+     */
     @Test
     public void testBasicGetName() {
         //check that SniperTower has correct name
         assertEquals("Basic Tower", basicTower.getName());
     }
 
+    /**
+     * check that BasicTower get basic cost runs successfully
+     */
     @Test
     public void testBasicGetBasicCost() {
         //ensure that no error when getting basic cost
         basicTower.getBasicCost();
     }
 
+    /**
+     * checks MachineTower shoots a SmallProjectile
+     */
     @Test
     public void testMachineTowerShoot() {
         assertTrue(machineTower.shoot() instanceof SmallProjectile);
     }
 
+    /**
+     * checks that MachineTower implements get name method correctly
+     */
     @Test
     public void testMachineGetName() {
         //check that SniperTower has correct name
         assertEquals("Machine Tower", machineTower.getName());
     }
 
+    /**
+     * checks that MachineTower get basic cost method runs successfully
+     */
     @Test
     public void testMachineGetBasicCost() {
         //ensure that no error when getting basic cost
         machineTower.getBasicCost();
     }
 
+    /**
+     * checks Tower set normal health method by using basicTower as concrete implementation
+     */
     @Test
     public void testSetNormalHealth() {
         //set to a random health and just make sure that default health is not equal to the random health
@@ -79,16 +109,28 @@ public class TowerTest {
         assertEquals(34, basicTower.getHealth());
     }
 
+    /**
+     * checks tower set zero health returns error and health is not changed
+     */
     @Test
     public void testSetZeroHealth() {
+        int defaultHealth = basicTower.getHealth();
         //tower should not have zero health
         assertEquals(-1, basicTower.setHealth(0));
+        //check health not changed
+        assertEquals(defaultHealth, basicTower.getHealth());
     }
 
+    /**
+     * checks tower set negative health returns -1 and health not changed
+     */
     @Test
     public void testSetNegativeHealth() {
+        int defaultHealth = basicTower.getHealth();
         //tower should not have negative health
         assertEquals(-1, basicTower.setHealth(-1));
+        //check health not changed
+        assertEquals(defaultHealth, basicTower.getHealth());
     }
 
 }
