83df91db735195350be515ed0e3bfb8093f0f7b4
czming <chng.zheming@dhs.sg>
2021-10-16 11:12:18 -0400

    add Sniper tower

---
+++ src/main/java/com/example/towerdefence/objects/SniperProjectile.java
@@ -0,0 +1,15 @@
+package com.example.towerdefence.objects;
+
+public class SniperProjectile extends Projectile{
+
+    /**
+     * constructor for NormalProjectile which has damage 10, speed 1 and "" as imagePath
+     */
+    public SniperProjectile() {
+        super(20, 2, "");
+    }
+
+    @Override
+    public void hitTarget(Enemy e) { }
+
+}
---
+++ src/main/java/com/example/towerdefence/objects/SniperTower.java
@@ -0,0 +1,7 @@
+package com.example.towerdefence.objects;
+
+public class SniperTower extends Tower {
+    public SniperTower() {
+        super(100, 1, SniperProjectile.class, "");
+    }
+}
