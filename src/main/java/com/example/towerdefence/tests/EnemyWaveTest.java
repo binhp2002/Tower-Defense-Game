1a45ba6af49dedfaa52abef42e777e86b317cc3d
czming <chng.zheming@dhs.sg>
2021-10-29 20:51:03 -0400

    add EnemyWaveTest

---
+++ src/main/java/com/example/towerdefence/tests/EnemyWaveTest.java
@@ -0,0 +1,18 @@
+package com.example.towerdefence.tests;
+
+import com.example.towerdefence.objects.*;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.hamcrest.CoreMatchers.instanceOf;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertThat;
+import static org.junit.Assert.assertArrayEquals;
+import static org.junit.Assert.assertTrue;
+
+public class EnemyWaveTest {
+
+    EnemyWave enemyWave;
+
+
+}
