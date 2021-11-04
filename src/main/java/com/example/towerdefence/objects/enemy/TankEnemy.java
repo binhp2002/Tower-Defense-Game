1e35074efccc697b9f08a9e6b19d88693cb4c139
czming <zchng3@gatech.edu>
2021-11-03 22:58:05 -0400

    add TankEnemy

---
+++ src/main/java/com/example/towerdefence/objects/enemy/TankEnemy.java
@@ -0,0 +1,30 @@
+package com.example.towerdefence.objects.enemy;
+
+public class TankEnemy extends Enemy{
+
+    public static final String IMAGE_PATH = "";
+
+    /**
+     * create TankEnemy with default [0, 0] location
+     */
+    public TankEnemy() {
+        this(0, 0);
+    }
+
+    /**
+     * specify location of enemy to be created, default health of 200 and default damage of 20
+     *
+     * @param x x coordinate of enemy
+     * @param y y coordinate of enemy
+     */
+    public TankEnemy(int x, int y) {
+        super(200, 20, 10, x, y);
+    }
+
+    @Override
+    public String getImagePath() {
+        return IMAGE_PATH;
+    }
+
+
+}
